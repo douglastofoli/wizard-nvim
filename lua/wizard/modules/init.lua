32c6219c6f435c09658201cda3d4ea6cc2a7720f
douglastofoli <tofoli.douglas@hotmail.com>
2022-04-24 19:22:46 -0300

    change wrong word on notify plugin

--- lua/wizard/modules/init.lua
+++ lua/wizard/modules/init.lua
@@ -1,26 +1,25 @@
 -- Wizard Nvim Modules
 
-local is_plugin_disabled = require("wizard.utils")
--- local use_floating_win_packer = require("doom.core.config").config.doom.use_floating_win_packer
+local system = require("wizard.core.system")
+local is_plugin_disabled = require("wizard.utils").is_plugin_disabled
+local use_window_floating_packer = require("wizard.core.config").config.use_window_floating_packer
 
 -- Download Packer
-
-local packer_path = vim.fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
-
 if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
-  --require("doom.extras.logging").info("Bootstrapping packer.nvim, please wait ...")
+  -- require("wizard.extras.logging").info("Bootstrapping packer.nvim, please wait ...")
   vim.fn.system({
     "git",
     "clone",
     "https://github.com/wbthomason/packer.nvim",
-    packer_path,
+    vim.fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim",
   })
 end
 
--- Load packer
+-- Load Packer
 vim.cmd([[ packadd packer.nvim ]])
 local packer = require("packer")
 
+-- Change some defaults
 packer.init({
   git = {
     clone_timeout = 300, -- 5 mins
@@ -31,7 +30,7 @@ packer.init({
     },
   },
   display = {
-    open_fn = use_floating_win_packer and function()
+    open_fn = use_window_floating_packer and function()
       return require("packer.util").float({ border = "single" })
     end or nil,
   },
@@ -41,91 +40,168 @@ packer.init({
 })
 
 packer.startup(function(use)
-  -- Core ---
+  ------------------
+  --     Core     --
+  ------------------
 
-  -- Plugin manager
+  -- wbthomason/packer.nvim - A use-package inspired plugin manager for Neovim
+  -- https://github.com/wbthomason/packer.nvim
   use({
     "wbthomason/packer.nvim",
     opt = true,
   })
 
+  -- nvim-lua/plenary.nvim - All the lua functions I don't want to write twice.
+  -- https://github.com/nvim-lua/plenary.nvim
   use({
     "nvim-lua/plenary.nvim",
-    -- commit = pin_commit("563d9f6d083f0514548f2ac4ad1888326d0a1c66"),
     module = "plenary",
   })
 
-  -- Snippets
-  local disabled_snippets = is_plugin_disabled("snippets")
+  -- -- Disabled LSP/CMP
+  local disabled_lsp = is_plugin_disabled("lsp")
 
-  -- Autopairs
-  local disabled_autopairs = is_plugin_disabled("autopairs")
+  -----------------
+  --     CMP     --
+  -----------------
 
-  -- CMP
+  -- hrsh7th/nvim-cmp - A completion engine plugin for neovim written in Lua
+  -- https://github.com/hrsh7th/nvim-cmp
   use({
     "hrsh7th/nvim-cmp",
     wants = { "LuaSnip" },
-    requires = {
-      {
-        "L3MON4D3/LuaSnip",
-        event = "BufReadPre",
-        wants = "friendly-snippets",
-        config = require("wizard.modules.config.luasnip"),
-        disable = disabled_snippets,
-        requires = { "rafamadriz/friendly-snippets" },
-      },
-      {
-        "windwp/nvim-autopairs",
-         config = require("wizard.modules.config.autopairs"),
-         disable = disabled_autopairs,
-         event = "BufReadPre",
-      },
-    },
     config = require("wizard.modules.config.cmp"),
-    disable = disabled_lsp,
     event = "InsertEnter",
+    disable = disabled_lsp,
   })
+
+  -- hrsh7th/cmp-nvim-lua - nvim-cmp source for nvim lua
+  -- https://github.com/hrsh7th/cmp-nvim-lua
   use({
     "hrsh7th/cmp-nvim-lua",
-    disable = disabled_lsp,
     after = "nvim-cmp",
+    disable = disabled_lsp,
   })
+
+  -- hrsh7th/cmp-path - nvim-cmp source for path
+  -- https://github.com/hrsh7th/cmp-path
   use({
     "hrsh7th/cmp-path",
     after = "nvim-cmp",
+    disable = disabled_lsp,
   })
+
+  -- hrsh7th/cmp-nvim-lsp - nvim-cmp source for neovim builtin LSP client
+  -- https://github.com/hrsh7th/cmp-nvim-lsp
   use({
     "hrsh7th/cmp-nvim-lsp",
     after = "nvim-cmp",
-  })
-  use({
-    "hrsh7th/cmp-buffer",
     disable = disabled_lsp,
-    after = "nvim-cmp",
-  })
-  use({
-    "saadparwaiz1/cmp_luasnip",
-    disable = disabled_lsp,
-    after = "nvim-cmp",
   })
 
-  -- LSP
-  local disabled_lsp = is_plugin_disabled("lsp")
+  -- hrsh7th/cmp-buffer - nvim-cmp source for buffer words
+  -- https://github.com/hrsh7th/cmp-buffer
+  use({
+    "hrsh7th/cmp-buffer",
+    after = "nvim-cmp",
+    disable = disabled_lsp,
+  })
+
+  -- saadparwaiz1/cmp_luasnip - luasnip completion source for nvim-cmp
+  -- https://github.com/saadparwaiz1/cmp_luasnip
+  use({
+    "saadparwaiz1/cmp_luasnip",
+    after = "nvim-cmp",
+    disable = disabled_lsp,
+  })
+
+  -----------------
+  --     LSP     --
+  -----------------
+
+  -- neovim/nvim-lspconfig - Quickstart configurations for the Nvim LSP client
+  -- https://github.com/neovim/nvim-lspconfig
   use({
     "neovim/nvim-lspconfig",
     config = require("wizard.modules.config.lspconfig"),
     disable = disabled_lsp,
   })
 
+  use({
+    "williamboman/nvim-lsp-installer",
+    config = require("wizard.modules.config.lsp-installer"),
+    after = "nvim-lspconfig",
+    disable = disabled_lsp,
+  })
 
-  -- UI
+  -- jose-elias-alvarez/null-ls.nvim - Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+  -- https://github.com/jose-elias-alvarez/null-ls.nvim
+  use({
+    "jose-elias-alvarez/null-ls.nvim",
+    after = "nvim-lspconfig",
+    disable = disabled_lsp,
+  })
+
+  -- tamago324/nlsp-settings.nvim - A plugin for setting Neovim LSP with JSON or YAML files
+  -- https://github.com/tamago324/nlsp-settings.nvim
+  use({
+    "tamago324/nlsp-settings.nvim",
+    after = "nvim-lspconfig",
+    disable = disabled_lsp,
+  })
+
+  ----------------------
+  --     Snippets     --
+  ----------------------
+
+  -- LuaSnip
+  local disabled_snippets = is_plugin_disabled("snippets")
+  use({
+    "L3MON4D3/LuaSnip",
+    event = "BufReadPre",
+    wants = "friendly-snippets",
+    config = require("wizard.modules.config.luasnip"),
+    requires = { "rafamadriz/friendly-snippets" },
+    disable = disabled_snippets,
+  })
+
+  --------------------
+  --     Editor     --
+  --------------------
+
+  -- Autopairs
+  local disabled_autopairs = is_plugin_disabled("autopairs")
+  use({
+    "windwp/nvim-autopairs",
+    config = require("wizard.modules.config.autopairs"),
+    event = "BufReadPre",
+    disable = disabled_autopairs,
+  })
+
+  ----------------
+  --     UI     --
+  ----------------
 
   -- Dashboard
   local disabled_dashboard = is_plugin_disabled("dashboard")
   use({
     "glepnir/dashboard-nvim",
+    config = require("wizard.modules.config.dashboard"),
     disable = disabled_dashboard,
   })
 
-  -- Custom Plugins
+  -- rcarriga/nvim-notify - A fancy, configurable, notification manager for NeoVim
+  -- https://github.com/rcarriga/nvim-notify
+  local disabled_notify = is_plugin_disabled("notify")
+  use({
+    "rcarriga/nvim-notify",
+    config = require("wizard.modules.config.notify"),
+    disable = disabled_notify,
+  })
+
+  local custom_plugins = require("wizard.core.config").plugins
+  -- Custom plugins
+  for _, plug in pairs(custom_plugins or {}) do
+    packer.use(plug)
+  end
 end)
